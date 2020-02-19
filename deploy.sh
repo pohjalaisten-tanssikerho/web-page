9c0577055b0dc0dc74a479848cd7c824dd8f560d
Greatman Lim <greatman.lim@mahti.org>
2020-02-19 12:36:01 +0200

    Fix path

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 git pull --ff-only
 hugo
-PASSWORD=$(pass internet/kwander@porthan.osakunta.fi)
+PASSWORD=$(pass pot/kwander@porthan.osakunta.fi)
 lftp -c "open -u kwander,$PASSWORD sftp://porthan.osakunta.fi; set ftp:ssl-allow yes; set ssl:verify-certificate no; mirror -R public /group/other/tanssikerho/public_html"
 rm -r public
