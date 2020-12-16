f815f96c6c2dd5b3d43d134f37dc750efa140a24
Greatman Lim <greatman.lim@mahti.org>
2020-12-16 15:20:38 +0200

    Add dynamic to form

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -1,21 +1,39 @@
-console.log('JS loaded! Woohoo!')
+// import firebase from 'firebase/app'
+// import 'firebase/firestore'
+//
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDbXlTKDMPsTEMnAyDZ31AmNF_ssmk2b54",
+//   authDomain: "pot-members.firebaseapp.com",
+//   projectId: "pot-members",
+//   storageBucket: "pot-members.appspot.com",
+//   messagingSenderId: "338810087970",
+//   appId: "1:338810087970:web:4f65d5e78c88b6ce06787b",
+//   measurementId: "G-E2JM0QJ3H2"
+// };
+// // Initialize Firebase
+// const firebaseApp = firebase.initializeApp(firebaseConfig)
+// export const db = firebaseApp.firestore()
+//
+// const getDoc = db.collection('members').doc('2021k')
 
-import firebase from 'firebase/app'
-import 'firebase/firestore'
+const toggleHidden = (elementsById) => {
+  elementsById.forEach( e => document.getElementById(e).classList.toggle('hidden'))
+}
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDbXlTKDMPsTEMnAyDZ31AmNF_ssmk2b54",
-  authDomain: "pot-members.firebaseapp.com",
-  projectId: "pot-members",
-  storageBucket: "pot-members.appspot.com",
-  messagingSenderId: "338810087970",
-  appId: "1:338810087970:web:4f65d5e78c88b6ce06787b",
-  measurementId: "G-E2JM0QJ3H2"
-};
-// Initialize Firebase
-const firebaseApp = firebase.initializeApp(firebaseConfig)
-export const db = firebaseApp.firestore()
+const displayHidden = (elementsById) => {
+  elementsById.forEach( e => document.getElementById(e).classList.remove('hidden'))
+}
 
-let getDoc = db.collection('members').doc('2021k')
+const hide = (elementsById) => {
+  elementsById.forEach( e => document.getElementById(e).classList.add('hidden'))
+}
+
+const amountWarning = () => {
+  const amount = document.getElementById('support-amount').value
+  const warning = document.getElementById('amount-warning')
+  if (amount < 10) warning.classList.remove('hidden')
+  else warning.classList.add('hidden')
+  console.log(amount)
+}
