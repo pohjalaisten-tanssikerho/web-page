5683cdc55e2979463ead68f00eb4133f5022d827
Greatman Lim <greatman.lim@mahti.org>
2022-03-21 18:53:18 +0200

    Test scheduler

--- netlify/functions/build-daily.js
+++ netlify/functions/build-daily.js
@@ -1,6 +1,6 @@
 const { schedule } = require('@netlify/functions')
 
-const handler = async function() {
+const handler = async function(event, context) {
   fetch('https://api.netlify.com/build_hooks/622c7a05e6bcc356aeae7a63', {
     method: 'POST'
   })
